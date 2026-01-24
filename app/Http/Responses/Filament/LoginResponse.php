4abb4d1d69c98b548f2624afd0477ed876b5a1b9
Victor BRILLANT <victor.brillant.ext@versusmind.eu>
2026-01-24 13:58:57 +0100

    fix

--- app/Http/Responses/Filament/LoginResponse.php
+++ app/Http/Responses/Filament/LoginResponse.php
@@ -5,10 +5,11 @@ namespace App\Http\Responses\Filament;
 use Filament\Facades\Filament;
 use Filament\Http\Responses\Auth\Contracts\LoginResponse as LoginResponseContract;
 use Illuminate\Http\RedirectResponse;
+use Livewire\Features\SupportRedirects\Redirector;
 
 class LoginResponse implements LoginResponseContract
 {
-    public function toResponse($request): RedirectResponse
+    public function toResponse($request): RedirectResponse|Redirector
     {
         $panel = Filament::getCurrentPanel();
         $panelPath = '/' . ltrim($panel->getPath(), '/');
